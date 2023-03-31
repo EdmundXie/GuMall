58639db9d8d6c1098594615cfd3b9841d570af53
EdmundXie <609031809@qq.com>
2023-03-31 16:32:53 +0100

    introduce Nacos as config center and gateway as exposure interface

---
+++ gumall-gateway/src/main/java/com/edm/gumall/gateway/GumallGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.edm.gumall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GumallGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumallGatewayApplication.class, args);
+	}
+
+}
