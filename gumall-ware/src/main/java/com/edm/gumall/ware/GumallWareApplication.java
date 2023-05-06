1f12b523bc31cc2ed1c3bbe15b45a82847f71a44
EdmundXie <609031809@qq.com>
2023-05-06 12:25:51 +0100

    update gumall-ware

--- gumall-ware/src/main/java/com/edm/gumall/ware/GumallWareApplication.java
+++ gumall-ware/src/main/java/com/edm/gumall/ware/GumallWareApplication.java
@@ -2,8 +2,12 @@ package com.edm.gumall.ware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableTransactionManagement
 public class GumallWareApplication {
 
     public static void main(String[] args) {
